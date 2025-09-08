22d68a04d770dd8eefa61f4bf27f413da787be40
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2025-09-08 17:54:45 -0300

    se suben los datos de la note del trabajo

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,13 +1,16 @@
-import { SimpleWidget } from "@/components/SimpleWidget";
+import { WidgetsGrid } from "@/components/dashboard/WidgetsGrid";
+
+export const metadata = {
+  title: "Admin Dashboard",
+  description: "SEO Title",
+};
 
 export default function MainPage() {
   return (
     <div className="text-black p-2">
       <h1 className="mt-2 text-3xl">Dashboard</h1>
       <span className="text-xl">Informacion General</span>
-      <div className="flex flex-wrap p-2 items-center justify-center">
-        <SimpleWidget />
-      </div>
+      <WidgetsGrid />
     </div>
   );
 }
